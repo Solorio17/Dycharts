bd13adbfcc0e73dff5d89a1cff54bdee1a0c9fb7
Jason <jasonsoloriom@gmail.com>
2018-08-23 13:35:45 -0600

    Completely Deleting the first line chart, and created a new one. Topic is weather forecast for popular cities in colorado. It has a dataset which customizes the whole look of the chart, it also has options which target the scale of the yAxis. And also has customization for the legend and title

--- js/dycharts.js
+++ js/dycharts.js
@@ -1,31 +1,107 @@
+// const CHART = document.getElementById("lineChart");
+// let lineChart = new Chart(CHART, {
+//       type: 'line',
+//       data: {
+//         labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+//         datasets: [{
+//           label: "Months",
+//           fill: true,
+//           lineTension: 0.1,
+//           borderColor: "rgb(14, 100, 175)",
+//           backgroundColor: "rgb(181, 10, 10)",
+//           data: [
+//             {x:10,y:30}, {x: 30,y: 70}
+//           ]
+//         }],
+//         datasets: [{
+//           yAxisID: 'Temperatures',
+//           xAxisIS: "Months"
+//         }]
+//       },//data
+//       options:{
+//         scales: {
+//           yAxes: [{
+//             id: 'Temperatures',
+//             type: 'linear',
+//             ticks: {
+//               min: 0,
+//               max: 90,
+//             }
+//           }],
+//           xAxes:{
+//             id: "Months",
+//             type: 'linear',
+//             ticks: {
+//               min: 0,
+//               max: 10
+//             }
+//           }
+//         }
+//       }//options
+//     }//linechart
+// );
+
+
 const CHART = document.getElementById("lineChart");
+Chart.defaults.global.defaultFontColor = 'black';
+Chart.defaults.global.defaultFontFamily = 'Arial';
+
 let lineChart = new Chart(CHART, {
-      type: 'line',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
-        datasets: [{
-          label: "Months",
-          fill: true,
-          lineTension: 0.1,
-          borderColor: "rgb(14, 100, 175)",
-          backgroundColor: "rgb(181, 10, 10)",
-          data: [65, 59, 80, 81, 56, 55, 40, 50]
-        }],
-        datasets: [{
-          yAxisID: 'Temperatures'
-        }]
-      },//data
-      options:{
-        scales: {
-          yAxes: [{
-            id: 'Temperatures',
-            type: 'linear',
-            ticks: {
-              min: 0,
-              max: 90
-            }
-          }]
+  type: 'line',
+  data: {
+    labels:['Denver',
+            'Greeley',
+            'Aurora',
+            'Boulder',
+            'Thornton',
+            'Golden',
+            'Lakewood',
+            'Fort Collins',
+            'Aspen',
+            'Vail'
+          ],
+    datasets:[{
+      label: 'Temperatures for populated cities',
+      data: [99, 95, 78, 67, 12, 37, 44, 86, 23, 0],
+      backgroundColor: 'rgba(22, 106, 170, 0.8)',
+      borderColor: 'black',
+      borderWidth: '4',
+      fill: true,
+      pointBackgroundColor: 'black',
+      pointBorderWidth: '1',
+    }],
+    // datasets: [{
+    //   yAxisID: "Temperatures"
+    // }]
+  },
+  options: {
+    scales:{
+      yAxes: [{
+        gridLines:{
+          display: false,
+          drawTicks: true
+        },
+        label: 'Temp',
+        scaleLabel: {
+          display: true,
+          labelString: 'Temperature(F)',
+        },
+        id: "Temperatures",
+        type: "linear",
+        ticks: {
+          min: 0,
+          max: 100,
+          stepSize: 10
         }
-      }
-    }//linechart
-);
+      }]
+    },
+    legend:{
+      display: false
+    },
+    title:{
+      display: true,
+      text: 'Weather Forecast',
+      position: 'top'
+    }
+  }
+})
